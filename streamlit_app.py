60e9ac2b3c8c44fadbb0300e8976543a8d4ed2d5
Yuri <75639439+yuridmayerpro@users.noreply.github.com>
2022-10-18 18:16:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import date, datetime
 import altair as alt
-
+alt.themes.enable("streamlit")
 st.title('Teste - Título')
 
 
@@ -10,22 +10,21 @@ dados = pd.read_csv('final_processed_df_streamlit.csv', encoding='iso-8859-1', s
 dados.loc[:, 'mês'] = pd.to_datetime(dados['mês'])
 
 ################################### Filtros ###################################
-col1, col2, = st.columns(2)
+col1 = st.columns(1)
 
-with col1:
+'''with col1:
     start_date = st.date_input(
         "Select start date",
         date(2017, 1, 1),
         min_value=datetime.strptime("2017-01-01", "%Y-%m-%d"),
         max_value=datetime.now(),
-    )
+    )'''
 
-with col2:
+with col1:
     time_frame = st.selectbox(
         "Produtos", ('Produto A', 'Produto B', 'Produto C', 'Produto D', 'Produto E', 'Produto F', 'Produto G', 'Produto H', 'Produto I')
     )
-
     
 ################################### Gráfico ###################################
-df_filtrado = dados[(dados.ds_subcategoria == time_frame) & (dados['mês'] >= start_date.strftime("%Y-%m-%d"))][['mês', 'real']].set_index('mês')                
-st.line_chart(df_filtrado)
+df_filtrado = dados[(dados.ds_subcategoria == time_frame) & (dados['mês'] >= start_date.strftime("%Y-%m-%d"))][['mês', 'real']].set_index('mês')
+st.line_chart(filtra(dados), use_container_width=True)
