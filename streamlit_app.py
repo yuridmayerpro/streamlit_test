ef1e1a6fb21d3400bfce009f4e7fef6f369aad0f
Yuri <75639439+yuridmayerpro@users.noreply.github.com>
2022-10-18 18:35:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ st.title('Teste - Título')
 
 dados = pd.read_csv('final_processed_df_streamlit.csv', encoding='iso-8859-1', sep=';')
 dados.loc[:, 'mês'] = pd.to_datetime(dados['mês'])
-dados.loc[:, 'real'] = dados['real'].astype('int')
-dados.loc[:, 'resultado_modelo'] = dados['resultado_modelo'].astype('int')
+dados.loc[:, 'real'] = dados['real'].astype('float')
+dados.loc[:, 'resultado_modelo'] = dados['resultado_modelo'].astype('float')
 dados.sort_values('mês', inplace=True)
 
 
