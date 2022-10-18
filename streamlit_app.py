5aeb436e8b8b54db5e6a1c1bd30a82683a6c5868
Yuri <75639439+yuridmayerpro@users.noreply.github.com>
2022-10-18 18:23:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,22 +10,21 @@ dados = pd.read_csv('final_processed_df_streamlit.csv', encoding='iso-8859-1', s
 dados.loc[:, 'mês'] = pd.to_datetime(dados['mês'])
 
 
-col1, col2 = st.columns(2)
+
 
 
 ################################### Filtros ###################################
+col1, col2 = st.columns(2)
+
 with col1:
     start_date = st.date_input(
         "Select start date",
         date(2017, 1, 1),
         min_value=datetime.strptime("2017-01-01", "%Y-%m-%d"),
-        max_value=datetime.now())
-    
+        max_value=datetime.now(),)
 
 with col2:
-    time_frame = st.selectbox(
-        "Produtos", ('Produto A', 'Produto B', 'Produto C', 'Produto D', 'Produto E', 'Produto F', 'Produto G', 'Produto H', 'Produto I')
-    )
+    time_frame = st.selectbox("Produtos", ('Produto A', 'Produto B', 'Produto C', 'Produto D', 'Produto E', 'Produto F', 'Produto G', 'Produto H', 'Produto I'))
     
         
 ################################### Gráfico ###################################
