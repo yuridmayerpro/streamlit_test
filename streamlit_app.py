a62d3aabbeab9a1bc232b9a4d1ad23c219471d47
Yuri <75639439+yuridmayerpro@users.noreply.github.com>
2022-10-18 18:31:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,4 +30,5 @@ with col2:
         
 ################################### Gráfico ###################################
 df_filtrado = dados[(dados.ds_subcategoria == time_frame) & (dados['mês'] >= start_date.strftime("%Y-%m-%d"))][['mês', 'real', 'resultado_modelo']]
-st.line_chart(data=df_filtrado, x='mês', y=['real', 'resultado_modelo'])
+#st.line_chart(data=df_filtrado, x='mês', y=['real', 'resultado_modelo'])
+st.line_chart(x=df_filtrado['mês'].values, y=df_filtrado['real'].values)
