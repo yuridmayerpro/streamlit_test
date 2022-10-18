cee82ac4404a99b728064cf50a7662bb85dc6798
Yuri <75639439+yuridmayerpro@users.noreply.github.com>
2022-10-18 17:18:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,23 @@ st.title('Teste - Título')
 
 
 dados = pd.read_csv('final_processed_df_streamlit.csv', encoding='iso-8859-1')
+data_corte = 
+
+
+col1, col2, = st.columns(2)
+
+with col1:
+    start_date = st.date_input(
+        "Select start date",
+        date(2017, 1, 1),
+        min_value=datetime.strptime("2020-01-01", "%Y-%m-%d"),
+        max_value=datetime.now(),
+    )
+
+with col2:
+    time_frame = st.selectbox(
+        "Produtos", ('Produto A', 'Produto B', 'Produto C', 'Produto D', 'Produto E', 'Produto F', 'Produto G', 'Produto H', 'Produto I')
+    )
+
 
 
